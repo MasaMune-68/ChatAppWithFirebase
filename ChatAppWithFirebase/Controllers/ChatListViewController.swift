1a588fd03965ccc6ef707e1208cc1511127adbac
MasaMune-68 <masamunedazeze@gmail.com>
2021-09-02 04:50:35 +0900

    09.02 04:50

--- ChatAppWithFirebase/Controllers/ChatListViewController.swift
+++ ChatAppWithFirebase/Controllers/ChatListViewController.swift
@@ -4,12 +4,24 @@ import Firebase
 class ChatListViewController: UIViewController {
     
     private let cellId = "cellId"
-    private var users = [User]()
+    private var user: User? {
+        didSet {
+            navigationItem.title = user?.username
+        }
+    }
     
     @IBOutlet weak var chatListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
+        confirmLoggedInUser()
+        fetchLoginUserInfo()
+        
+    }
+    
+    private func setupViews() {
         
         chatListTableView.delegate = self
         chatListTableView.dataSource = self
@@ -17,6 +29,14 @@ class ChatListViewController: UIViewController {
         navigationController?.navigationBar.barTintColor = .rgb(red: 39, green: 49, blue: 69)
         navigationItem.title = "トーク"
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+
+        let rigntBarButton = UIBarButtonItem(title: "新規チャット", style: .plain, target: self, action: #selector(tappedNavRightBarButton))
+        navigationItem.rightBarButtonItem = rigntBarButton
+        navigationItem.rightBarButtonItem?.tintColor = .white
+        
+    }
+    
+    private func confirmLoggedInUser() {
         
         if Auth.auth().currentUser?.uid == nil {
             
@@ -26,36 +46,33 @@ class ChatListViewController: UIViewController {
             self.present(signUpViewController, animated: true, completion: nil)
             
         }
+        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    @objc private func tappedNavRightBarButton() {
+        let storyboard = UIStoryboard.init(name: "UserList", bundle: nil)
+        let userListViewController = storyboard.instantiateViewController(withIdentifier: "UserListViewController")
+        let nav = UINavigationController(rootViewController: userListViewController)
+        self.present(nav, animated: true, completion: nil)
         
-        fetchUserInfoFromFirestore()
     }
     
-    private func fetchUserInfoFromFirestore() {
+    private func fetchLoginUserInfo() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
         
-        Firestore.firestore().collection("users").getDocuments { (snapshots, err) in
+        Firestore.firestore().collection("users").document(uid).getDocument { (snapshot, err) in
             if let err = err {
-                print("user情報の取得に失敗しました。\(err)")
+                print("ユーザー情報の取得に失敗しました。\(err)")
                 return
             }
             
-            snapshots?.documents.forEach({ (snapshot) in
-                let dic = snapshot.data()
-                let user = User.init(dic: dic)
-                
-                self.users.append(user)
-                self.chatListTableView.reloadData()
-                
-                self.users.forEach { (user) in
-                    print("user.username: ", user.username)
-                }
-            })
+            guard let snapshot = snapshot, let dic = snapshot.data() else { return }
             
+            let user = User(dic: dic)
+            self.user = user
         }
     }
+    
 }
 
 
@@ -66,13 +83,11 @@ extension ChatListViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = chatListTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ChatListTableViewCell
-        cell.user = users[indexPath.row]
-        
         return cell
     }
     
---
+++ ChatAppWithFirebase/StoryBoards/UserList.swift
@@ -0,0 +1,20 @@
+//
+//  UserList.swift
+//  ChatAppWithFirebase
+//
+//  Created by 高橋政宗 on 2021/09/02.
+//
+
+import SwiftUI
+
+struct UserList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserList_Previews: PreviewProvider {
+    static var previews: some View {
+        UserList()
+    }
+}
